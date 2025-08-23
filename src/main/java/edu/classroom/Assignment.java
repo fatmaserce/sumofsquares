76119f72bbd8f4afa68044c01df3c8c6cc38c3e4
cemileserce <cemileserce@gmail.com>
2025-08-22 22:56:57 -0700

    restored

--- src/main/java/edu/classroom/Assignment.java
+++ src/main/java/edu/classroom/Assignment.java
@@ -7,12 +7,7 @@ package edu.classroom;
  */
 public class Assignment {
     public static int sumOfSquares(int n) {
-    	if(n <= 0) return 0;
-	int sum = 0;
-	for(int i = 1; i <= n ; i++){
-	   sum += i * i;
-	}
 
-	return sum;
+	return -1;
     }
 }
